1f6d9632644fbb4dbb5a09a1026f6fdf51f6bacc
elisheval <elisheva2272@gmail.com>
2023-10-31 22:04:08 +0200

    tryy-server

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
  
-// Initializations
 const app = express();
 require('./config/passport');
 
